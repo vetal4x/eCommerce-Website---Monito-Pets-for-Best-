1ff9dae51899d03e49c2f676f39cb75ae68562ac
vetal4x <vetal4x@gmail.com>
2025-04-05 22:53:57 +0200

    added some hover effects, imported new font files

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
   // Mobile Menu
   document.addEventListener('DOMContentLoaded', () => {
-    const hamburgerButton = document.querySelector('.hamburger-button');
-    const mobileMenu = document.querySelector('.mobile-menu');
-  
-    hamburgerButton.addEventListener('click', () =>
-      mobileMenu.classList.toggle('active')
-    );
+    const hamburgerButton = document.querySelector('.hamburger__button');
+    const mobileMenu = document.querySelector('.mobile__menu');
+    console.log(hamburgerButton);
+  hamburgerButton.addEventListener('click', () => {
+      mobileMenu.classList.toggle('active');
+  });
   });
