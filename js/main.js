2a181f64e1462d7f30766647329175992bd64f2d
vetal4x <vetal4x@gmail.com>
2025-04-05 23:13:27 +0200

    fixed mobile menu (hide after scroll)

--- js/main.js
+++ js/main.js
@@ -2,8 +2,22 @@
   document.addEventListener('DOMContentLoaded', () => {
     const hamburgerButton = document.querySelector('.hamburger__button');
     const mobileMenu = document.querySelector('.mobile__menu');
-    console.log(hamburgerButton);
-  hamburgerButton.addEventListener('click', () => {
-      mobileMenu.classList.toggle('active');
+    let menuVisible = false; // Состояние меню
+  
+    hamburgerButton.addEventListener('click', () => {
+      menuVisible = !menuVisible;
+      mobileMenu.classList.toggle('active', menuVisible);
+    });
+  
+    let prevScrollpos = window.scrollY;
+    window.addEventListener('scroll', () => {
+      const currentScrollPos = window.scrollY;
+      if (menuVisible && prevScrollpos < currentScrollPos) {
+        // Скрыть меню при прокрутке вниз
+        mobileMenu.style.left = "-70vw";
+        menuVisible = false; // Меню теперь скрыто
+      }
+      prevScrollpos = currentScrollPos;
+    });
   });
-  });
+  
